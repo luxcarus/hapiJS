61f7387ecbb7269cb4cfb392fbd314c22068a469
luxcarus <swluxlee@gmail.com>
2018-03-12 22:36:18 +0900

    temp

--- server/routes/gate.js
+++ server/routes/gate.js
@@ -7,7 +7,7 @@ module.exports = {
 			// reply.file('gate/signin.html')
 			// reply.file('/')
 			// reply('hello world ~~~ *')
-			Path.join(__dirname, 'gate/signin.html')
+			reply.file(Path.join(__dirname, 'gate/signin.html'))
 		}
   },
   signin: {
